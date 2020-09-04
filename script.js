19f63eb8c906aeafd3a7a122e48e74f2a7fa1109
Flavio Alessandri <flavio.alessandri@gmail.com>
2020-09-04 22:21:52 +0200

    Update Script.js containing 2 Ajax calls

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 
-
-
-function init(){
-
-
+// new function ------------------------------------------
 function getAjaxData(){
-
   $.ajax(
     {
       url:"http://localhost/www/php-dischi/datajs.php",
@@ -28,8 +23,50 @@ function getAjaxData(){
 
             var compileHTML = compiled(datas[i]);
             target.append(compileHTML);
+          }
+        }
 
+      },
+      error: function(err){
+        console.log("errore",err);
+      }
+  });
+}
 
+// new function ------------------------------------------
+function getCardsByGenre(){
+  $('#container').html("");
+  var input = $("#input").val();
+  const genre = input.charAt(0).toUpperCase() + input.slice(1);
+  $("#input").val("");
+
+  $.ajax(
+    {
+      url:"http://localhost/www/php-dischi/datajs.php" ,
+      method:"GET",
+      success: function (state,data) {
+        console.log("state",state);
+        console.log("data",data);
+
+        if(data == "success"){
+
+          var datas = state['response'];
+
+          var template = $("#template").html();
+          var compiled = Handlebars.compile(template);
+          var target = $('#container');
+          console.log("template",template);
+
+          for (var i = 0; i < datas.length; i++) {
+
+            if(datas[i]["genre"]==genre){
+              var compileHTML = compiled(datas[i]);
+              target.append(compileHTML);
+            }
+            else if (genre.length==0) {
+              var compileHTML = compiled(datas[i]);
+              target.append(compileHTML);
+            }
 
           }
         }
@@ -38,17 +75,22 @@ function getAjaxData(){
       error: function(err){
         console.log("errore",err);
       }
-
-
-
-
   });
 }
+
+// new function ------------------------------------------
+function init(){
+
+  var btn = $("#btn");
+
+
+    getAjaxData();
+
+  btn.click(getCardsByGenre);
 
 
   console.log("hello world");
 
-  getAjaxData();
 }
 
 $(document).ready(init);
